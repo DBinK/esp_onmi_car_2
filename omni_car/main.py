ce7d8f8146c39c4633cb65a315ed5883bd8c5323
DBinK <DBinKv1@Gmail.com>
2025-04-12 15:02:40 +0800

    refactor(omni_car): 优化小车控制逻辑和数据处理
    - 调整上电等待时间，从 2 秒改为 1 秒
    - 添加缩放因子以调节小车运动速度
    - 修改数据处理流程，增加数据有效性检查
    - 优化代码结构，提高可读性和可维护性

--- omni_car/main.py
+++ omni_car/main.py
@@ -7,7 +7,7 @@ import modules.now_recv as now
 from modules.motion import RobotChassis
 from modules.utils import TimeDiff, map_value, limit_value
 
-time.sleep(2)  # 防止上电停不下来程序
+time.sleep(1)  # 防止上电停不下来程序
 
 LED=Pin(15,Pin.OUT) #构建led对象，GPIO46,输出
 LED.value(1) #点亮LED，也可以使用led.on()
@@ -15,19 +15,24 @@ LED.value(1) #点亮LED，也可以使用led.on()
 motor_pins = [1, 2, 14, 13, 38, 36, 8, 10]
 robot = RobotChassis(motor_pins)
 
+scale_x = 0.8
+scale_y = 0.8
+scale_w = 0.4
 
 while True:
-        data = now.read_espnow()
+    data = now.read_espnow()
+    data = now.process_data(data)
 
-        if data:
-            print(data)
-            if data[0] == 1:
-                if data[1] == 0 and data[2] == 0 and data[3] == 0 and data[4] == 0:
-                    robot.stop()
-                else:
-                    robot.move(data[1], data[2], data[3])
+    if data:
+        print(data)
+        if data[1] > 10 and data[2] > 10 and data[3] > 10 and data[4] > 10:
+            robot.stop()
+        else:
+            robot.move(-data[1]*scale_x, data[2]*scale_y, -data[3]*scale_w)
+    else:
+        robot.stop()
 
-        time.sleep(0.01)
+    time.sleep(0.01)
 
     # robot.turn_left(40)
     # time.sleep(3)
---
+++ omni_car/modules/encoder.py
@@ -0,0 +1,79 @@
+# encoder_timed.py
+
+# Copyright (c) 2016-2021 Peter Hinch
+# Released under the MIT License (MIT) - see LICENSE file
+# Improvements provided by IhorNehrutsa
+
+import time
+from machine import Pin, disable_irq, enable_irq
+
+class Encoder:
+    def __init__(self, pin_x, pin_y, scale=1):
+        self.scale = scale  # Optionally scale encoder rate to distance/angle
+        self.tprev = -1
+        self.tlast = -1
+        self.forward = True
+        self.pin_x = pin_x
+        self.pin_y = pin_y
+        self._pos = 0
+        try:
+            self.x_interrupt = pin_x.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.x_callback, hard=True)
+            self.y_interrupt = pin_y.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.y_callback, hard=True)
+        except TypeError:
+            self.x_interrupt = pin_x.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.x_callback)
+            self.y_interrupt = pin_y.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.y_callback)
+
+    def x_callback(self, line):
+        self.forward = self.pin_x.value() ^ self.pin_y.value()
+        self._pos += 1 if self.forward else -1
+        self.tprev = self.tlast
+        self.tlast = time.ticks_us()
+
+    def y_callback(self, line):
+        self.forward = self.pin_x.value() ^ self.pin_y.value() ^ 1
+        self._pos += 1 if self.forward else -1
+        self.tprev = self.tlast
+        self.tlast = time.ticks_us()
+
+    def rate(self):  # Return rate in signed distance/angle per second
+        state = disable_irq()
+        tlast = self.tlast  # Cache current values
+        tprev = self.tprev
+        enable_irq(state)
+        if self.tprev == -1:  # No valid times yet
+            return 0.0
+        dt = time.ticks_diff(time.ticks_us(), tlast)
+        if dt > 10_000: # Stopped
+            return 0.0
+        dt = time.ticks_diff(tlast, tprev)
+        if dt == 0:  # Could happen on future rapid hardware
+            return 0.0
+        result = self.scale * 1_000_000.0/dt
+        return result if self.forward else -result
+
+    def position(self, value=None):
+        if value is not None:
+            self._pos = round(value / self.scale)
+        return self._pos * self.scale
+
+    def reset(self):
+        self._pos = 0
+
+    def value(self, value=None):
+        if value is not None:
+            self._pos = value
+        return self._pos
+
+if __name__ == '__main__':
+
+    from machine import Pin
+    import time
+
+    AH1 = Pin(4, Pin.IN, Pin.PULL_UP)
+    AH2 = Pin(6, Pin.IN, Pin.PULL_UP)
+
+    encoder = Encoder(AH1, AH2)
+
+    while True:
+        print(f"rate={encoder.rate():.2f}, pos={encoder.position():.2f}")
+        time.sleep(0.01)
